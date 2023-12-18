10d329da1ae2f38ad220e26fcd41be43f510753d
unknown <jcarmor0807@g.educaand.es>
2023-12-18 12:22:10 +0100

    Principal.java creado

---
+++ Principal.java
@@ -0,0 +1,9 @@
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//Este fichero será el que realiza la función clase inicial del proyecto algún día
+	}
+
+}
